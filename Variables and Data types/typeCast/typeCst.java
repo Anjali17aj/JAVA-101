9eb42715f01a47a0c0756cedcd6bc2decebd29be
Anjali17aj <sayhellotoanjali@gmail.com>
2025-04-20 10:37:54 +0530

    Variables and data types

---
+++ Variables and Data types/typeCast/typeCst.java
@@ -0,0 +1,8 @@
+
+public class typeCst {
+    public static void main(String args[]){
+        float a = 25.12f;
+        int b = (int)a;
+        System.out.println("The value of a is: " + b);
+    }
+}
