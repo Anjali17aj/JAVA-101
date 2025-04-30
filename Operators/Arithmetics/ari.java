4b95c88c1ce60fc0b440a6358ecba2b4984f5d32
Anjali17aj <sayhellotoanjali@gmail.com>
2025-04-30 09:37:13 +0530

    Operators

---
+++ Operators/Arithmetics/ari.java
@@ -0,0 +1,12 @@
+public class ari{
+    public static void main(String args[]){
+        int a=5;
+        int b=10;
+
+        System.out.println("add="+ (a+b)); // addition
+        System.out.println("sub="+ (a-b)); // subtraction   
+        System.out.println("mul="+ (a*b)); // multiplication
+        System.out.println("div="+ (a/b)); // division
+        System.out.println("mod="+ (a%b)); // modulus
+    }
+}
