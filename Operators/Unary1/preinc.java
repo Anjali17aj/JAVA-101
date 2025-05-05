b469a67f35ba7bb2e53b2d3dc8a6b9e4d2d3282f
Anjali17aj <sayhellotoanjali@gmail.com>
2025-05-05 09:58:03 +0530

    Operators

---
+++ Operators/Unary1/preinc.java
@@ -0,0 +1,10 @@
+// Java program to demonstrate the use of increment operator
+// in Java
+public class preinc {
+    public static void main(String args[]){
+        int a =5;
+        int b =++a;
+        System.out.println(a); 
+        System.out.println(b);
+    }
+}
