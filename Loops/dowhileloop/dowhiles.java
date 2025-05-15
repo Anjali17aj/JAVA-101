1ba42568ce984d07bf3fbda6be931113561cf1db
Anjali17aj <sayhellotoanjali@gmail.com>
2025-05-15 09:44:06 +0530

    Loops

---
+++ Loops/dowhileloop/dowhiles.java
@@ -0,0 +1,10 @@
+public class dowhiles {
+    public static void main(String args[]){
+int count=1;
+do{
+    System.out.println("hello");
+    count++;
+    } while(count<=10);
+    System.out.println("done");
+    }
+}
