e1bc89c8389ca8412eca0546d2ebf440f4de14d1
Anjali17aj <sayhellotoanjali@gmail.com>
2025-05-27 10:18:46 +0530

    FunctionsAndMethods

---
+++ FunctionsAndMethods/IntroFunction/introFunc.java
@@ -0,0 +1,10 @@
+public class introFunc {
+
+    public static void printHello() {
+        System.out.println("Hello, World!");
+    }
+    public static void main(String args[]) {
+     printHello();
+     // Calling the function
+}
+}
